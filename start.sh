43a6fc5fb7e8f69ff58d87f3d98f8220c00a4e7e
xana-b <alexander.bergner@xana.at>
2025-03-10 11:11:17 +0100

    First working version

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export DISPLAY=:0
+export XAUTHORITY=/home/xana/.Xauthority
+USER_DIR="/app"
+
+rm -rf ~/.cache/chromium
+
+node-red -f x_tstst/flow/controller.json &
+sleep 10
+
+    chromium --kiosk \
+        --fullscreen \
+        --start-maximized \
+        "http://localhost:1880/ui"
